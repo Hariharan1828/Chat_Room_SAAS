269a085d41c535dc22d0aca074089032d9a15d48
Hariharan1828 <hariharanr.yr@gmail.com>
2024-03-14 11:34:31 +0530

    icon check

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ const Header = async () => {
             
             }
 
-            <CreateChatButton isLarge/>
+            <CreateChatButton isLarge={false}/>
             <DarkModeToggle/>
             <UserButton Session={Session}/>
           </div>
